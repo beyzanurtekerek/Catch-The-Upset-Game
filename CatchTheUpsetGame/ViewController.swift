4808fd95079763d840c206238192865a908b096a
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-07-30 07:08:19 +0300

    Initial Commit

---
+++ CatchTheUpsetGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheUpsetGame
+//
+//  Created by Beyza Nur Tekerek on 30.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
